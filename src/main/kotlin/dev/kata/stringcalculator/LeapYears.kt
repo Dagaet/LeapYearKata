316031e82ea6fbc60c88f4bf9ca1f176a1edb70f
Dagaet <alojera15@gmail.com>
2024-03-19 09:17:04 +0000

    Third test "Not be leap year if divisible by 4 but not 100" in green

--- src/main/kotlin/dev/kata/stringcalculator/LeapYears.kt
+++ src/main/kotlin/dev/kata/stringcalculator/LeapYears.kt
@@ -3,6 +3,9 @@ package dev.kata.stringcalculator
 class LeapYears {
     fun isLeapYear(year: Int): Boolean
     {
+        if (year % 4 == 0 && year % 100 != 0){
+            return true
+        }
         if (year % 400 == 0 && year % 100 != 0){
             return false
         }
